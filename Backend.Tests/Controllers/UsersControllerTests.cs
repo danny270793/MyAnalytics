660642b9e8e2f1885eca9d6ae329584eeea70700
Danny Vaca <dvaca@technisys.com>
2025-12-05 23:08:38 -0500

    test(backend): add UsersController tests

---
+++ Backend.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Backend.Controllers;
+using Backend.Data;
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Tests;
+
+public class UsersControllerTests
+{
+    private AppDbContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new AppDbContext(options);
+    }
+
+    [Fact]
+    public async Task GetUsers_ReturnsOkResult()
+    {
+        var dbContext = GetInMemoryDbContext();
+        var controller = new UsersController(dbContext);
+
+        var result = await controller.GetUsers();
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = result as OkObjectResult;
+        Assert.IsType<List<User>>(okResult.Value);
+        var users = okResult.Value as List<User>;
+        Assert.Equal(0, users.Count);
+    }
+}
